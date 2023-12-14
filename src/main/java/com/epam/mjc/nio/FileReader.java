e1611f23acf64deb149741d2efe42f959ee20dfb
ulugbe7 <ulugbekyusupov272@gmail.com>
2023-12-14 16:33:45 +0500

    finish

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,62 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+
+        String nameLine = "";
+        String ageLine = "";
+        String emailLine = "";
+        String phoneLine = "";
+        StringBuilder build = new StringBuilder();
+
+        try (RandomAccessFile aFile = new RandomAccessFile(file, "r");
+             FileChannel inChannel = aFile.getChannel()) {
+            long fileSize = inChannel.size();
+
+            ByteBuffer buffer = ByteBuffer.allocate((int) fileSize);
+            inChannel.read(buffer);
+            buffer.flip();
+
+            int i = 0;
+            while (i < fileSize) {
+                while ((char) buffer.get() != '\n') {
+                    build.append((char) buffer.get(i));
+                    if (i < (fileSize - 1))
+                        i++;
+                }
+                if (build.toString().contains("Name")) {
+                    nameLine = build.toString();
+                    build.setLength(0);
+                } else if (build.toString().contains("Age")) {
+                    ageLine = build.toString();
+                    build.setLength(0);
+                } else if (build.toString().contains("Phone")) {
+                    phoneLine = build.toString();
+                    build.setLength(0);
+                } else if (build.toString().contains("Email")) {
+                    emailLine = build.toString();
+                    build.setLength(0);
+                }
+                i++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        String name = nameLine.substring(nameLine.indexOf(':') + 2);
+        String age = ageLine.substring(ageLine.indexOf(':') + 2);
+        String email = emailLine.substring(emailLine.indexOf(':') + 2);
+        String phone = phoneLine.substring(phoneLine.indexOf(':') + 2);
+
+        return new Profile(name.trim(), Integer.valueOf(age.trim()), email.trim(), Long.valueOf(phone.trim()));
     }
 }
